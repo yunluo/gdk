888321a721af8b5b80f2094ae066dfa0149b0c63
云落 <sp91@qq.com>
2021-01-16 22:24:32 +0800

    一些更新

--- gdk.php
+++ gdk.php
@@ -3,10 +3,10 @@
 Plugin Name: GitCafe Development Kit 极客公园开发套件
 Plugin URI: https://gitcafe.net/
 Description: 为WordPress主题开发提供底层支持
-Version: 0.0.3
+Version: 1.0
 Author: 云落
 Author URI: https://gitcafe.net/
-Compatible:5.3.2
+Compatible:5.5.1
  */
 
 //万能的开头
@@ -16,19 +16,19 @@ if (!defined('ABSPATH')) {exit;}
 function deactivate_myself()
 {
     deactivate_plugins(plugin_basename(__FILE__));
-    wp_die('启动失败，GDK插件不兼容Git主题且需要运行在 PHP 7.2 版本及更高的环境下。');
+    wp_die('启动失败，GDK插件不兼容Git主题且需要运行在 PHP 7.3 版本及更高的环境下。');
 }
 
 if (defined('GIT_URL')) {
     add_action('update_option_active_plugins', 'deactivate_myself');
 }
 
-if (!version_compare(PHP_VERSION, '7.2', '>=')) {
+if (!version_compare(PHP_VERSION, '7.3', '>=')) {
     add_action('update_option_active_plugins', 'deactivate_myself');
 }
 
 //定义各种常量
-define('GDK_PLUGIN_VER', '0.0.3');
+define('GDK_PLUGIN_VER', '1.0');
 define('GDK_PLUGIN_FILE', __FILE__); //插件入口文件
 define('GDK_BASE_URL', plugin_dir_url(__FILE__)); //插件目录url
 define('GDK_ROOT_PATH', plugin_dir_path(__FILE__)); //插件目录路径
@@ -38,5 +38,4 @@ include 'framework/frame_load.php'; //加载后台框架
 include 'class/class_load.php'; //加载各种类
 include 'functions/func_load.php'; //加载函数
 include 'assets/assets_load.php'; //加载静态资源
-
 
